dd900d0b02f6a1c3d8e6f363228b488f872961b7
PoisonK12 <sadriancampos.10@gmail.com>
2023-09-22 02:09:37 -0400

    Server funcionando

--- Server/index.js
+++ Server/index.js
@@ -4,9 +4,9 @@ const { sequelize } = require('./src/database/db');
 
 const port = process.env.PORT || 3001
 
-sequelize.sync({ alter: true }).then(() => {
-  console.log('Database synchronized successfully.');
+// sequelize.sync({ alter: true }).then(() => {
+//   console.log('Database synchronized successfully.');
   server.listen(port, () => {
     console.log(`Server listening at ${port}... it's working!`)
   });
-});
+// });
